efa326a87c164b0a52259d79cc4dcaaf753f7d42
Hack Reactor Students <ethoreby@gmail.com>
2014-03-24 16:43:26 -0700

    Created new db table for username and password

--- app/config.js
+++ app/config.js
@@ -41,6 +41,21 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
   }
 });
 
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knew.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('username', 30);
+      user.string('password', 30);
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+
+
+
 /************************************************************/
 // Add additional schema definitions below
 /************************************************************/
